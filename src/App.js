961a68a7bafb5a9e7f6854b47d77553f23d6c1e1
Ashley Chen <ashleyc2219@gmail.com>
2022-03-04 13:59:41 +0800

    連接資料庫 01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import config from './Config'
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:3500/address-book/api/list')
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+      setData(data)})
+  }, []);
+
+  // 會跑兩次 1st:最初始render頁面 
+  // 2nd:因為12行setData，狀態值改變，所以會再render一次
+  console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{data.rows}</div>
+    </>
   );
 }
 
