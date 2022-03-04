f100b29fc230fd006ab7bd424e681b3faa32238d
Ashley Chen <ashleyc2219@gmail.com>
2022-03-04 15:27:02 +0800

    改寫成async await

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-import config from './Config'
+// 將連接資料庫需要用到的參數，丟到一個檔案裡方便管理
+import config from './Config';
 
 function App() {
-  const [data, setData] = useState([])
-  useEffect(()=>{
-    fetch('http://localhost:3500/address-book/api/list')
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data);
-      setData(data)})
+  const [data, setData] = useState([]);
+  // 改寫成async, await的方式
+  useEffect(() => {
+    (async () => {
+      const r1 = await fetch(config.AB_LIST);
+      const obj = await r1.json();
+      console.log(obj);
+      setData(obj);
+    })();
   }, []);
 
-  // 會跑兩次 1st:最初始render頁面 
+  // 會跑兩次 1st:最初始render頁面
   // 2nd:因為12行setData，狀態值改變，所以會再render一次
-  console.log(data)
+  console.log(data);
+
+  // 判斷data拿到沒有，如果是undefined的時候render就會報錯
+  const renderMe = (data) => {
+    if (data.rows && data.rows.length) {
+      return data.rows.map((value) => (
+        <tr key={'test' + value.pro_id}>
+          <td>{value.pro_id}</td>
+          <td>{value.pro_name}</td>
+          <td>{value.pro_stock}</td>
+          <td>{value.pro_condition}</td>
+        </tr>
+      ));
+    } else {
+      return '';
+    }
+  };
+
   return (
-    <>
-      <div>{data.rows}</div>
-    </>
+    <div className="App">
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">name</th>
+              <th scope="col">stock</th>
+              <th scope="col">condition</th>
+            </tr>
+          </thead>
+          <tbody>{renderMe(data)}</tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
---
+++ src/Config.js
@@ -0,0 +1,6 @@
+const host = 'http://localhost:3500';
+const config = {
+    AB_LIST: `${host}/address-book/api/list`
+
+};
+export default config;
