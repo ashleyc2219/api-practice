39c4df2d5b448ab486b0af438ca48fa187626da3
Ashley Chen <ashleyc2219@gmail.com>
2022-03-07 11:50:54 +0800

    admin 上傳圖片 API

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,7 @@
 const host = 'http://localhost:3500';
 const config = {
-    AB_LIST: `${host}/address-book/api/list`
-
+  AB_LIST: `${host}/address-book/api/list`,
+  Admin_LIST: `${host}/admin/myfrom`,
+  IMG_PATH: `${host}/img`,
 };
 export default config;
